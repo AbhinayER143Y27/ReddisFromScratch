633722668f03af2d608507bc2985a7dac491172a
Abhinay Rana <abhinayrana46@gmail.com>
2026-08-22 17:46:31 +0530

    Implement ECHO command and enhance thread logic
    Added support for ECHO command and improved thread handling.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Main
@@ -10,7 +12,7 @@ class Main
         try (ServerSocket serversocket = new ServerSocket(port)) {
             while (true) {
                 Socket socket = serversocket.accept();
-                Thread thread = new Thread(() -> {
+                Thread thread = new Thread(() -> {//for the 10,000 threads the CPU will spend more time swapping between threads than actually doing the work - context switching overhead.
                     try {
                         System.out.println("The connection has been build");
                         InputStream stream = socket.getInputStream();
@@ -20,16 +22,43 @@ class Main
                         while (true) {
                             String line = reader.readLine();
                             if (line == null) break;
+
                             if (line.equals("COMMAND")) {
                                 output.write("+OK\r\n".getBytes());
                                 output.flush();
                             }
+                            if (line.equals("exit")) {
+                                break;
+                            }
+
+                            if(line.startsWith("*"))
+                            {
+                                int arrayNumber = Integer.parseInt(line.substring(1));
+                                ArrayList<String> collectedArgs = new ArrayList<>();
+                                for(int i = 0; i < 2 * arrayNumber; i++)
+                                {
+                                    String insideLine = reader.readLine();
+                                    if(insideLine.startsWith("$"))
+                                    {
+                                        continue;
+                                    }
+                                    else{
+                                        collectedArgs.add(insideLine);
+                                    }
+                                }
+
+                                if(collectedArgs.get(0).equals("ECHO"))
+                                {
+                                    for(int i = 1; i < collectedArgs.size(); i++)
+                                    {
+                                        output.write(("$" + collectedArgs.get(i).length() + "\r\n").getBytes());
+                                        output.write((collectedArgs.get(i) + "\r\n").getBytes());
+                                    }
+                                }
+                            }
                             if (line.equals("PING")) {
                                 output.write("+PONG\r\n".getBytes());
                                 output.flush();
-                            }
-                            if (line.equals("exit")) {
-                                break;
                             }
                             System.out.println("The users request: " + line);
                         }
