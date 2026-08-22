47eae309b61b8287707e29bf55d4a22518f9ce8a
Abhinay Rana <abhinayrana46@gmail.com>
2026-08-22 14:52:56 +0530

    Created the project multi threaded

--- Main.java
+++ Main.java
@@ -8,33 +8,41 @@ class Main
     public static void main(String[] args) {
         int port = 6379;
         try (ServerSocket serversocket = new ServerSocket(port)) {
+            while (true) {
                 Socket socket = serversocket.accept();
-                System.out.println("The connection has been build");
-                InputStream stream = socket.getInputStream();
-                OutputStream output = socket.getOutputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                Thread thread = new Thread(() -> {
+                    try {
+                        System.out.println("The connection has been build");
+                        InputStream stream = socket.getInputStream();
+                        OutputStream output = socket.getOutputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
-                while(true) {
-                    String line = reader.readLine();
-                    if(line == null) break;
-                    if(line.equals("COMMAND"))
-                    {
-                        output.write("+OK\r\n".getBytes());
-                        output.flush();
+                        while (true) {
+                            String line = reader.readLine();
+                            if (line == null) break;
+                            if (line.equals("COMMAND")) {
+                                output.write("+OK\r\n".getBytes());
+                                output.flush();
+                            }
+                            if (line.equals("PING")) {
+                                output.write("+PONG\r\n".getBytes());
+                                output.flush();
+                            }
+                            if (line.equals("exit")) {
+                                break;
+                            }
+                            System.out.println("The users request: " + line);
+                        }
                     }
-                    if(line.equals("PING"))
+                    catch(IOException e)
                     {
-                        output.write("+PONG\r\n".getBytes());
-                        output.flush();
+                        System.out.println(e.getMessage());
                     }
-                    if(line.equals("exit"))
-                    {
-                        break;
-                    }
-                    System.out.println("The users request: " + line);
-                }
-        } catch (IOException e) {
-            System.out.println("Error : " + e.getMessage());
+                });
+                thread.start();
+            }
+        } catch(IOException e){
+                System.out.println("Error : " + e.getMessage());
         }
     }
 }
