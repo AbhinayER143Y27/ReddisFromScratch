16b93dcfd33b4d98412733582a94e7f4dabd8cfe
Abhinay Rana <abhinayrana46@gmail.com>
2026-08-22 12:17:59 +0530

    Created the PING PONG
    In this commit I have committed the connection between the Server and the host to send and get request for a specific data chain.

--- Main.java
+++ Main.java
@@ -7,17 +7,32 @@ class Main
 {
     public static void main(String[] args) {
         int port = 6379;
-        Scanner scanner = new Scanner(System.in);
         try (ServerSocket serversocket = new ServerSocket(port)) {
-            while (true) {
                 Socket socket = serversocket.accept();
                 System.out.println("The connection has been build");
                 InputStream stream = socket.getInputStream();
-
+                OutputStream output = socket.getOutputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                System.out.println("The users request: " + reader.readLine());
-                InputStream input = socket.getInputStream();
-            }
+
+                while(true) {
+                    String line = reader.readLine();
+                    if(line == null) break;
+                    if(line.equals("COMMAND"))
+                    {
+                        output.write("+OK\r\n".getBytes());
+                        output.flush();
+                    }
+                    if(line.equals("PING"))
+                    {
+                        output.write("+PONG\r\n".getBytes());
+                        output.flush();
+                    }
+                    if(line.equals("exit"))
+                    {
+                        break;
+                    }
+                    System.out.println("The users request: " + line);
+                }
         } catch (IOException e) {
             System.out.println("Error : " + e.getMessage());
         }
