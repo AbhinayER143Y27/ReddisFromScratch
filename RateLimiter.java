9cef1be9a9c56587862c5b348d0e0f0e120af098
Abhinay Rana <abhinayrana46@gmail.com>
2026-09-13 18:06:13 +0530

    Life Cycle of RateLimiter
    In this there is the lifecycle that has been covered and there is another thing to notice is that there is tryAcquire which is currently in development.

--- RateLimiter.java
+++ RateLimiter.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class RateLimiter {
     static final String HOST = "localhost";
@@ -10,15 +8,16 @@ public class RateLimiter {
     Socket socket;
     InputStream inputStream;
     OutputStream outputStream;
+    BufferedReader dataFromServer;
 
-
-    public RateLimiter(String words)
+    public RateLimiter()
     {
         try {
             socket = new Socket(HOST, PORT);
             inputStream = socket.getInputStream();
             outputStream = socket.getOutputStream();
-
+            dataFromServer = new BufferedReader(new InputStreamReader(inputStream)); // this will be the part of the get command which will be called many times
+            //making each call wrap every input stream into a brand new buffered reader which then actually does its own internal buffering making things go worse.
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -38,6 +37,42 @@ public class RateLimiter {
             outputStream.flush();
         }
         catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void tryAcquire() // skip this i am creating this currently
+    {
+        String key = null;
+        try {
+            key = dataFromServer.readLine();
+            sendCommand("Get", key);
+            String ans = getCommand();
+            if(ans == "$-1")
+            {
+                // we will put the bucket to max value;
+            }
+            else
+            {
+                ans.split(":");
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    String getCommand()
+    {
+        try
+        {
+            String dataReturned = dataFromServer.readLine();
+            System.out.println(dataReturned);
+            return dataReturned;
+        }
+        catch (Exception e)
         {
             throw new RuntimeException(e);
         }
